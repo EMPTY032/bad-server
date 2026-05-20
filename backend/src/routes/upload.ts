3ce708834260d235a092c8d5f063bec3472ccbbc
dima <komiksmr99@gmail.com>
2026-05-20 14:07:34 +0300

    test7.1

--- backend/src/routes/upload.ts
+++ backend/src/routes/upload.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express'
 import { uploadFile } from '../controllers/upload'
 import fileMiddleware from '../middlewares/file'
-
+import { v4 as uuidv4 } from 'uuid'
 const uploadRouter = Router()
 uploadRouter.post('/', fileMiddleware.single('file'), uploadFile)
 
